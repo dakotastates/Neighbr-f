38e62e5b955561f31f673d4a649e034ac1e0130e
Dakota States <dakota.states@gmail.com>
2020-10-20 15:46:01 -0700

    Redux Setup

---
+++ src/reducers/RootReducer.js
@@ -0,0 +1,11 @@
+
+import { combineReducers } from "redux";
+// import userReducers from "./usersReducer";
+// import AuthReducer from "./AuthReducer";
+// import commentsReducers from "./commentsReducer";
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
