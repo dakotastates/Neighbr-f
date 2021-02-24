c3188281b8f31ce3fe4a6a9b1f6a15ddc31307fa
Dakota States <dakota.states@gmail.com>
2021-02-24 12:48:17 -0800

    Connect Sidebar Users to modal and open profile.

---
+++ src/components/profile/UserDrawerList.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItem from '@material-ui/core/ListItem';
+import Avatar from '@material-ui/core/Avatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import ProfileChatModal from '../../helpers/ProfileChatModal'
+
+
+
+function UserDrawerList(props) {
+  // const [selectedUser, setSelectedUser] = useState({});
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+    <ListItem button key={props.selectedUser.id} onClick={handleOpen}>
+      <ListItemIcon>{props.selectedUser.profile.featured_image ? <Avatar alt={props.selectedUser.first_name} src={props.selectedUser.profile.featured_image.url}/> : <Avatar>{props.selectedUser.first_name.charAt(0)}</Avatar> }</ListItemIcon>
+      <ListItemText primary={props.selectedUser.first_name} />
+   </ListItem>
+   <ProfileChatModal open={open} onClose={handleClose} currentUser={props.currentUser} selectedUser={props.selectedUser}/>
+
+    </div>
+  )
+
+}
+
+
+export default UserDrawerList;
--- src/containers/UsersDrawerContainer.js
+++ src/containers/UsersDrawerContainer.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import Avatar from '@material-ui/core/Avatar';
 import { connect } from 'react-redux'
 import { storeUsers } from "../actions/UserActions";
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
-
+import UserDrawerList from '../components/profile/UserDrawerList'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -50,10 +50,7 @@ function UsersDrawerContainer(props) {
        } else {
           usersList = props.users.map(user =>{
            return(
-            <ListItem button key={user.id}>
-              <ListItemIcon>{user.profile.featured_image ? <Avatar alt={user.first_name} src={user.profile.featured_image.url}/> : <Avatar>{user.first_name.charAt(0)}</Avatar> }</ListItemIcon>
-              <ListItemText primary={user.first_name} />
-           </ListItem>
+             <UserDrawerList key={user.id} currentUser={props.user} selectedUser={user}/>
          )
          })
        }
@@ -69,6 +66,12 @@ function UsersDrawerContainer(props) {
 
 const mapStateToProps = (state) => ({
   users: state.usersStore.users,
+  user: state.usersStore.user
 });
 
 export default connect(mapStateToProps, {storeUsers})(UsersDrawerContainer);
+
+// <ListItem button key={user.id}>
+//   <ListItemIcon>{user.profile.featured_image ? <Avatar alt={user.first_name} src={user.profile.featured_image.url}/> : <Avatar>{user.first_name.charAt(0)}</Avatar> }</ListItemIcon>
+//   <ListItemText primary={user.first_name} />
+// </ListItem>
