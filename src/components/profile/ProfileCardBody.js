61340ee3e907802427e6313fe549298ad38dcc56
Dakota States <dakota.states@gmail.com>
2021-03-14 21:06:31 -0700

    Images List component

--- src/components/profile/ProfileCardBody.js
+++ src/components/profile/ProfileCardBody.js
@@ -11,6 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import ProfilePostCard from './ProfilePostCard'
 import ProfilePostForm from './ProfilePostForm'
 import NeighborshipsContainer from '../../containers/NeighborshipsContainer'
+import ImagesList from './ImagesList'
 
 
 const styles = theme => ({
@@ -97,18 +98,7 @@ function ProfileCardBody(props) {
                         <p class='font-italic mb-0'>{toggleForm.email ? <div> <EditUser data={"email"} updateUser={props.updateUser} profile={props.profile} toggle={setToggleForm}/></div> : <div> Email: {email} {toggleEdit ? <button name="email" onClick={handleEdit}>Edit Email</button> : null}<br/></div>}</p>
                     </div>
                 </div>
-                <div class="py-4 px-4">
-                    <div class="d-flex align-items-center justify-content-between mb-3">
-                        <h5 class="mb-0">Recent photos</h5><a href="#" class="btn btn-link text-muted">Show all</a>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6 mb-2 pr-lg-1"><img src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" class="img-fluid rounded shadow-sm" /></div>
-                        <div class="col-lg-6 mb-2 pl-lg-1"><img src="https://images.unsplash.com/photo-1493571716545-b559a19edd14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" class="img-fluid rounded shadow-sm" /></div>
-                        <div class="col-lg-6 pr-lg-1 mb-2"><img src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="" class="img-fluid rounded shadow-sm" /></div>
-                        <div class="col-lg-6 pl-lg-1"><img src="https://images.unsplash.com/photo-1475724017904-b712052c192a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" class="img-fluid rounded shadow-sm" /></div>
-                    </div>
-
-                </div>
+              <ImagesList />
                 <div class="px-4 py-3">
                     <ProfilePostForm currentUser={props.currentUser}/>
                     <ProfilePostCard />
